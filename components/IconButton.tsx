fc8fd1939a91fd96de4098075fcca7546c5a6c94
Baalamurgan <baalamurgan2307@gmailcom>
2022-05-20 14:53:59 +0530

    fix: icon hover color

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -6,7 +6,7 @@ const IconButton = ({ Icon, title, selected }: { Icon: any, title: string, selec
     <div className='font-bold cursor-pointer text-md'>
       <div
         className={classNames(
-          "flex gap-x-2 justify-center items-center ",
+          "flex gap-x-2 justify-center items-center hover:text-primary",
           [selected ? "color-primary border-b-[#7161C5] border-b-4" : "color-gray"]
         )}>
         <Icon className='inline h-5 w-5' aria-hidden="true" />
